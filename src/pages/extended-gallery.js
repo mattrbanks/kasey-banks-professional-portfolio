fd31576d084447edcd9c8853a3ee357375a8ca38
mattrbanks <banks.matt81@gmail.com>
2021-04-16 16:39:06 -0400

    Added two more videos to extended gallery page.

--- src/pages/extended-gallery.js
+++ src/pages/extended-gallery.js
@@ -53,7 +53,7 @@ const ExtendedGallery = () => {
                   </p>
                 </div>
                 <iframe
-                  src="https://drive.google.com/file/d/1gCOLYqeTUJLv7mqt01AsqwKpXAEl9Tdp/preview"
+                  src="https://www.youtube.com/embed/AxTeYRTzY8s"
                   width="640"
                   height="480"
                   title={"K-2 Module Lessons Overview"}
@@ -76,10 +76,10 @@ const ExtendedGallery = () => {
                   &nbsp;
                 </div>
                 <p className="text-xl">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Fugiat eos nihil, consectetur ab asperiores dolore sequi
-                  inventore doloremque quis iure ut ratione, eius nisi eum illo
-                  iusto laboriosam modi praesentium?
+                  I am using whiteboard.chat to virtually instruct a group of
+                  4th grade students on how to divide three-digit numbers by a
+                  one-digit number using place value methods aligned to the
+                  standard algorithm.
                 </p>
               </div>
 
@@ -91,9 +91,11 @@ const ExtendedGallery = () => {
                   margin: "6rem 0 6rem 0",
                 }}
               >
-                <p className="text-2xl">Jargon Title</p>
+                <p className="text-2xl">
+                  Facilitating a Data Team Meeting with 3rd Grade Teachers
+                </p>
                 <iframe
-                  src="https://drive.google.com/file/d/0B5FyHSpwBHIzakNxNlNLMVAwZDg/preview"
+                  src="https://www.youtube.com/embed/-nn6d4oKBH8"
                   width="640"
                   height="480"
                   title={"K-2 Module Lessons Overview"}
@@ -116,10 +118,9 @@ const ExtendedGallery = () => {
                   &nbsp;
                 </div>
                 <p className="text-xl">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Fugiat eos nihil, consectetur ab asperiores dolore sequi
-                  inventore doloremque quis iure ut ratione, eius nisi eum illo
-                  iusto laboriosam modi praesentium?
+                  We are engaging in an assessment preview protocol, in order to
+                  plan for upcoming instruction and to meet the various needs of
+                  the students.
                 </p>
               </div>
             </div>
@@ -133,9 +134,11 @@ const ExtendedGallery = () => {
                   margin: "6rem 0 6rem 0",
                 }}
               >
-                <p className="text-2xl">Jargon Title</p>
+                <p className="text-2xl">
+                  Coaching Cycle Routine: Debrief after Micro-Modeling
+                </p>
                 <iframe
-                  src="https://drive.google.com/file/d/0B5FyHSpwBHIzakNxNlNLMVAwZDg/preview"
+                  src="https://www.youtube.com/embed/rL6BbeNNeRI"
                   width="640"
                   height="480"
                   title={"K-2 Module Lessons Overview"}
@@ -158,10 +161,15 @@ const ExtendedGallery = () => {
                   &nbsp;
                 </div>
                 <p className="text-xl">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Fugiat eos nihil, consectetur ab asperiores dolore sequi
-                  inventore doloremque quis iure ut ratione, eius nisi eum illo
-                  iusto laboriosam modi praesentium?
+                  I am debriefing an observation with a third-grade teacher,
+                  after micro-modeling a math routine. The teacher was
+                  struggling to get students to participate in math. My goal was
+                  to show students will participate in the math if it is
+                  engaging and they know the expectations. The teacher was given
+                  a note catcher that outlined our goals for the coaching cycle
+                  and took down evidence of how I met those goals while working
+                  with her students. This video highlights a portion of our
+                  debrief.
                 </p>
               </div>
               <div
